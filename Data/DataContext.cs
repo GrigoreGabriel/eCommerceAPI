a692f281119f27ad6dfb1b99c1ac6ab8cf2a63d1
GrigoreGabriel <92084992+GrigoreGabriel@users.noreply.github.com>
2023-04-16 11:08:13 +0300

    init commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using eCommerceAPI.Data.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerceAPI.Data
+{
+    public class CommerceDbContext : DbContext
+    {
+        public CommerceDbContext(DbContextOptions<CommerceDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
